a77733731a04d7f57210032aa8df6d032d96271f
yh <poh1207@gmail.com>
2021-08-10 09:13:41 +0900

    chatting

--- server.c
+++ server.c
@@ -1,46 +1,92 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<arpa/inet.h>
-#include<sys/socket.h>
-#include<unistd.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 
-int main(int argc, char* argv[])
+#define BUF_LEN 128
+#define port 9000
+
+int main()
 {
-    int serv_sock;
-    int clint_sock;
+    char buffer[BUF_LEN];
+    struct sockaddr_in server_addr, client_addr;
+    char temp[20];
+    int server_fd, client_fd;
+    int len, msg_size;
+    char test[20];
+    
 
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in clint_addr;
-    socklen_t clnt_addr_size;
-
-    if (argc != 2)
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
-        printf("%s <port>\n", argv[0]);
-        //exit(1);
+        printf("Server: can not Open Socket\n");
+        exit(0);
     }
-    serv_sock = socket(PF_INET, SOCK_STREAM, 0); //1번
-    if (serv_sock == -1)
-        printf("socket error\n");
 
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(atoi(argv[1]));
+    memset(&server_addr, 0x00, sizeof(server_addr));
 
-    if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) //2번
-        printf("bind error\n");
-    if (listen(serv_sock, 5) == -1) //3번
-        printf("listen error\n");
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons( port);
 
-    clnt_addr_size = sizeof(clint_addr);
-    clint_sock = accept(serv_sock, (struct sockaddr*)&clint_addr, &clnt_addr_size); //4번
-    if (clint_sock == -1)
-        printf("accept error\n");
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
+    {
+        printf("Server: cat not bind local addrss\n");
+        exit(0);
 
-    char message[] = "hello client";
-   // write(clint_sock, message, sizeof(message)); //5번
-    close(serv_sock); //6번
-    close(clint_sock);
+    }
+
+
+    if (listen(server_fd, 5) < 0)
+    {
+        printf("Server: cat not listen connnect.\n");
+        exit(0);
+    }
+
+
+    memset(buffer, 0x00, sizeof(buffer));
+    len = sizeof(client_addr);
+    printf("Server : wating connection request.\n");
+
+
+    while (1)
+    {
+        client_fd = accept(server_fd, (struct sockaddr*)&client_addr, (socklen_t*)&len);
+
+        if (client_fd < 0)
+        {
+            printf("Server: accept failed\n");
+            exit(0);
+        }
+        inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, temp, sizeof(temp));
+        printf("Server: %s client connect.\n", temp);
+      
+        while (1)
+        {
+            recv(client_fd, (char*)buffer, BUF_LEN, 0);
+            printf("%s\n", buffer);
+
+            send(client_fd, "im server", 20, 0); // 1
+
+        }
+
+            send(client_fd, "test test test", 0xff, 0); // 1
+       
+        msg_size = recv(client_fd, (char*)buffer, BUF_LEN, 0);
+        send(client_fd, (char*)buffer, msg_size, 0);
+        close(client_fd);
+        printf("Server: %s client closed.\n", temp);
+
+
+
+    }
+
+    close(server_fd);
+
     return 0;
 }
